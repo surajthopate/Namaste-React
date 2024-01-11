8504fd0d2b78fc6ffd34b0103b4e062671ddd031
surajthopate24@gmail.com <surajthopate24@gmail.com>
2024-01-11 12:02:04 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,14 @@
+// const heading = React.createElement("h1", {id: "heading"}, "Hello World from React!");
+const parent = React.createElement("div", { id: "Parent" }, [
+    React.createElement("div", { id: "child" }, [
+      React.createElement("h1", { id: "h1" }, "Hello World from H1!"),
+      React.createElement("h2", { id: "h1" }, "Hello World from H2!"),
+    ]),
+    React.createElement("div", { id: "child2" }, [
+      React.createElement("h1", { id: "h1" }, "Hello World from H1!"),
+      React.createElement("h2", { id: "h1" }, "Hello World from H2!"),
+    ])
+  ]);
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(parent);
+  
