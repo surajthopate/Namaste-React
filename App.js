28e260aaef823ba83f9a1577731e3ba5458c1e5a
surajthopate24@gmail.com <surajthopate24@gmail.com>
2024-01-16 12:15:17 +0530

    episode-2

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 // const heading = React.createElement("h1", {id: "heading"}, "Hello World from React!");
 const parent = React.createElement("div", { id: "Parent" }, [
     React.createElement("div", { id: "child" }, [
-      React.createElement("h1", { id: "h1" }, "Hello World from H1!"),
+      React.createElement("h1", { id: "h1" }, "This is Namaste React !"),
       React.createElement("h2", { id: "h1" }, "Hello World from H2!"),
     ]),
     React.createElement("div", { id: "child2" }, [
--- ReadMe.md
+++ ReadMe.md
@@ -1,1 +1,19 @@
 # Namaste React
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR (Hot Module Replacement)
+- File Watching Algorithm - written in c++
+- Caching - Faster Builds
+- Image Optimization
+- Minification file
+- Bundling of files
+- Compress file size
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - supports older browsers
+- Diagnostics
+- Error Handling
+- HTTPS Hosting
+- Tree Shaking - remove unused code  
